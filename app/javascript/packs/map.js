ae270ecdc7ffaf86cb582f03f430354dda35f4e8
Abdelilah El hotmani <abdelilah.elhotmani@outlook.com>
2018-12-11 17:17:08 +0100

    views of add-instrument, add-message done + Jammins session changed view

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,7 +1,7 @@
 import 'mapbox-gl/dist/mapbox-gl.css'
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 const mapElement = document.getElementById('map');
-
+console.log(mapElement);
 if (mapElement) { // only build a map if there's a div#map to inject into
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   const map = new mapboxgl.Map({
