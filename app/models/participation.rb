272a356915541bafdb3b9494964825dd3e49ddab
Abdelilah El hotmani <abdelilah.elhotmani@outlook.com>
2018-12-14 15:15:55 +0100

    dependent :destroy

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,7 +2,7 @@ class Participation < ApplicationRecord
   belongs_to :jamm
   belongs_to :user
   belongs_to :instrument
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   validates :status, presence: :true, inclusion: { in: ["Pending", "Accepted", "Refused"] }
   attribute :status, :string, default: "Pending"
